04dc4f9b470f9eb52075164f8af5d03a12a853a0
wangjianglin <jianglin1>
2016-05-31 16:48:27 +0800

    修复app更新bug

--- core/core/Update/AppStoreUpdate.swift
+++ core/core/Update/AppStoreUpdate.swift
@@ -11,152 +11,142 @@ import UIKit
 import LinUtil
 
 public class AppStoreUpdate{
-
-
+    
+    struct YRSignal{
+        static var TIME_INTERVAL = 600.0;
+    }
+    
+    public class var interval:Double{
+        get{
+            return YRSignal.TIME_INTERVAL;
+        }
+        set{
+            YRSignal.TIME_INTERVAL = newValue;
+            if YRSignal.TIME_INTERVAL < 60.0 {
+                YRSignal.TIME_INTERVAL = 60.0;
+            }
+        }
+    }
+    
     public class func update(appId:String){
-        Queue.asynThread { 
+        Queue.asynThread {
             while(true){
                 updateImpl(appId);
-                NSThread.sleepForTimeInterval(600.0);
+                NSThread.sleepForTimeInterval(YRSignal.TIME_INTERVAL);
             }
         }
-//    [Queue asynThread:^{
-//        
-//        while (true) {
-//            [AppStoreUpdate updateImpl:appId];
-//            
-//            [NSThread sleepForTimeInterval:600.0];
-//        }
-//    }];
-}
-
-
-    private class func updateImpl(appId:String){
-
-//    let appUrl = "https://itunes.apple.com/lookup?id=\(appId)";
+    }
     
-    let url = NSURL(string:"https://itunes.apple.com/lookup?id=\(appId)");
-    let  request = NSMutableURLRequest();
-    request.URL = url;
-    let data = try? NSURLConnection.sendSynchronousRequest(request, returningResponse:nil);
-    if let data = data {
-        let updateVersion = String(data:data, encoding:NSUTF8StringEncoding);
-        
-        if updateVersion == nil {
-            return;
-        }
-        let json = Json.parse(updateVersion!);
+    
+    private class func updateImpl(appId:String){
         
         
-        let updateVersions = parserVersion(json["results"][0]["version"].asString(""));
-        if updateVersions == nil {
-            return;
-        }
-//        NSString * appVersion = [NSString stringWithFormat:@"%@",[[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleShortVersionString"]];
-        let appVersions = parserVersion(NSBundle.mainBundle().objectForInfoDictionaryKey("CFBundleShortVersionString") as? String);
-        if appVersions == nil {
-            return;
-        }
-        
-        var flag = 0;//0不需要更新，1、必须更新，2、非必须更新
-        
-        
-        if updateVersions[0] > appVersions[0] {
-            flag = 1;
-        }else if updateVersions[0] == appVersions[0] {
-            if updateVersions[1] > appVersions[1] {
+        let url = NSURL(string:"https://itunes.apple.com/lookup?id=\(appId)");
+        let  request = NSMutableURLRequest();
+        request.URL = url;
+        let data = try? NSURLConnection.sendSynchronousRequest(request, returningResponse:nil);
+        if let data = data {
+            let updateVersion = String(data:data, encoding:NSUTF8StringEncoding);
+            
+            if updateVersion == nil {
+                return;
+            }
+            let json = Json.parse(updateVersion!);
+            
+            
+            let updateVersions = parserVersion(json["results"][0]["version"].asString(""));
+            if updateVersions == nil {
+                return;
+            }
+            
+            let appVersions = parserVersion(NSBundle.mainBundle().objectForInfoDictionaryKey("CFBundleShortVersionString") as? String);
+            if appVersions == nil {
+                return;
+            }
+            
+            var flag = 0;//0不需要更新，1、必须更新，2、非必须更新
+            
+            
+            if updateVersions[0] > appVersions[0] {
                 flag = 1;
-            }else if updateVersions[1] == appVersions[1] {
-                if updateVersions[2] > appVersions[2] {
-                    flag = 2;
-                    //                    flag = 1;
+            }else if updateVersions[0] == appVersions[0] {
+                if updateVersions[1] > appVersions[1] {
+                    flag = 1;
+                }else if updateVersions[1] == appVersions[1] {
+                    if updateVersions[2] > appVersions[2] {
+                        flag = 2;
+                    }
                 }
             }
-        }
-        if (flag == 0) {
-            return;
-        }
-        //        if (flag == 2 && [UserDefaults[@"isupdateofversion"] isEqualToString:@"true"] && [UserDefaults[@"isupdateofversion_version"] isEqualToString:updateVersion]) {
-        //            return;
-        //        }
-        
-        var updateString:String!;
-        if (flag == 1) {
-            updateString = "应用有新版本，为了正常使用，请更新！";
-        }else{
-            updateString = "应用有新版本，是否需要更新！";
-        }
-        updateString = json["results"][0]["releaseNotes"].asString(updateString);
-        
-        Queue.mainQueue{ 
+            if (flag == 0) {
+                return;
+            }
             
-            var alert:UIAlertView! = nil;
             
-//            NSString *  urlString = [[NSString alloc] initWithFormat:@"https://itunes.apple.com/cn/app/wei/id%@",appId];
-            
-//            let url = NSURL(string:"https://itunes.apple.com/cn/app/wei/id\(appId)");
-            
-            if flag == 1 {
-                
-                alert = UIAlertView(title:"", message:updateString, delegate:nil, cancelButtonTitle:"更新");
-                alert.clickedButtonAtIndexAction = {(alertView:UIAlertView, buttonIndex:Int) in
-                    //                    if (buttonIndex == 1) {
-                    if let url = NSURL(string:"https://itunes.apple.com/cn/app/wei/id\(appId)") {
-                        UIApplication.sharedApplication().openURL(url);
-                    }
-                    //UserDefaults[@"isupdateofversion"] = @"false";
-                    //                    }else{
-                    //                        UserDefaults[@"isupdateofversion"] = @"true";
-                    //                        UserDefaults[@"isupdateofversion_version"] = updateVersion;
-                    //                    }
-                };
+            var updateString:String!;
+            if (flag == 1) {
+                updateString = "应用有新版本，为了正常使用，请更新！";
             }else{
-                alert = UIAlertView(title:"", message:updateString, delegate:nil, cancelButtonTitle:"取消", otherButtonTitles:"更新");
-                alert.clickedButtonAtIndexAction = {(alertView:UIAlertView, buttonIndex:Int) in
-                    if (buttonIndex == 1) {
-                        
+                updateString = "应用有新版本，是否需要更新！";
+            }
+            updateString = json["results"][0]["releaseNotes"].asString(updateString);
+            
+            Queue.mainQueue{
+                
+                var alert:UIAlertView! = nil;
+                
+                
+                if flag == 1 {
+                    
+                    alert = UIAlertView(title:"", message:updateString, delegate:nil, cancelButtonTitle:"更新");
+                    alert.clickedButtonAtIndexAction = {(alertView:UIAlertView, buttonIndex:Int) in
                         if let url = NSURL(string:"https://itunes.apple.com/cn/app/wei/id\(appId)") {
                             UIApplication.sharedApplication().openURL(url);
                         }
-                        //UserDefaults[@"isupdateofversion"] = @"false";
-                    }
-                };
-            }
-            
-            alert.willPresentAlertViewAction = {(alert:UIAlertView) in
-                for view in alert.subviews {
-                    
-                    if view is UILabel {
-                        (view as! UILabel).textAlignment = NSTextAlignment.Left;
+                    };
+                }else{
+                    alert = UIAlertView(title:"", message:updateString, delegate:nil, cancelButtonTitle:"取消", otherButtonTitles:"更新");
+                    alert.clickedButtonAtIndexAction = {(alertView:UIAlertView, buttonIndex:Int) in
+                        if (buttonIndex == 1) {
+                            
+                            if let url = NSURL(string:"https://itunes.apple.com/cn/app/wei/id\(appId)") {
+                                UIApplication.sharedApplication().openURL(url);
+                            }
+                        }
+                    };
+                }
+                
+                alert.willPresentAlertViewAction = {(alert:UIAlertView) in
+                    for view in alert.subviews {
+                        
+                        if view is UILabel {
+                            (view as! UILabel).textAlignment = NSTextAlignment.Left;
+                        }
                     }
                 }
-            }
+                
+                alert.show();
+                
+            };
             
-            alert.show();
-            
-        };
-        
-    }
-}
-
-    private class func parserVersion(version:String!)->[Int]!{
-    if version == nil || version == "" {
-        return nil;
-    }
-    let vs = version.componentsSeparatedByString(".");
-//    if (vs == nil) {
-//        return nil;
-//    }
-        
-    var arr = [Int](count:3,repeatedValue:0);
-    for n in 0 ..< 3 {
-        if n < vs.count {
-            arr[n] = (vs[n] as NSString).integerValue;
-        }else{
-            arr[n] = 0;
         }
     }
-    return arr;
-}
-
+    
+    private class func parserVersion(version:String!)->[Int]!{
+        if version == nil || version == "" {
+            return nil;
+        }
+        let vs = version.componentsSeparatedByString(".");
+        
+        var arr = [Int](count:3,repeatedValue:0);
+        for n in 0 ..< 3 {
+            if n < vs.count {
+                arr[n] = (vs[n] as NSString).integerValue;
+            }else{
+                arr[n] = 0;
+            }
+        }
+        return arr;
+    }
+    
 }
--- core/core/Update/EnterpriseUpdate.swift
+++ core/core/Update/EnterpriseUpdate.swift
@@ -10,94 +10,115 @@ import UIKit
 import LinUtil
 
 public class EnterpriseUpdate{
-
-    public class func update(appUrl:String, versionUrl:String){
     
-    let url = NSURL(string:versionUrl);
-    let request = NSMutableURLRequest();
-    request.URL = url;
-    let data = try? NSURLConnection.sendSynchronousRequest(request, returningResponse:nil);
-    if let data = data {
-        let updateVerString = String(data:data, encoding:NSUTF8StringEncoding);
-        
-        if updateVerString == nil {
-            return;
+    struct YRSignal{
+        static var TIME_INTERVAL = 600.0;
+    }
+    
+    public class var interval:Double{
+        get{
+            return YRSignal.TIME_INTERVAL;
         }
-        
-        let versionJson = Json.parse(updateVerString!)
-        let updateVersions = parserVersion(versionJson["version"].asString(""));
-        if updateVersions == nil {
-            return;
-        }
-        
-        let appVersions = parserVersion(NSBundle.mainBundle().objectForInfoDictionaryKey("CFBundleShortVersionString") as? String);
-        if appVersions == nil {
-            return;
-        }
-        
-        var flag = 0;//0不需要更新，1、必须更新，2、非必须更新
-        
-        
-        if updateVersions[0] > appVersions[0] {
-            flag = 1;
-        }else if updateVersions[0] == appVersions[0] {
-            if updateVersions[1] > appVersions[1] {
-                flag = 1;
-            }else if updateVersions[1] == appVersions[1] {
-                if updateVersions[2] > appVersions[2] {
-                    flag = 2;
-                }
+        set{
+            YRSignal.TIME_INTERVAL = newValue;
+            if YRSignal.TIME_INTERVAL < 60.0 {
+                YRSignal.TIME_INTERVAL = 60.0;
             }
         }
-        if (flag == 0) {
-            return;
+    }
+    
+    public class func update(appUrl:String, versionUrl:String){
+        Queue.asynThread {
+            while(true){
+                updateImpl(appUrl,versionUrl:versionUrl);
+                NSThread.sleepForTimeInterval(YRSignal.TIME_INTERVAL);
+            }
         }
+    }
+    private class func updateImpl(appUrl:String, versionUrl:String){
         
-        var updateString:String! = nil;
-        if (flag == 1) {
-            updateString = "应用有新版本，为了正常使用，请更新！";
-        }else{
-            updateString = "应用有新版本，是否需要更新！";
-        }
-        updateString = versionJson["message"].asString(updateString);
-        Queue.mainQueue(){
-            var alert:UIAlertView! = nil;
-            if flag == 1 {
-                
-                alert = UIAlertView(title:"", message:updateString, delegate:nil, cancelButtonTitle:"确定");
-                alert.clickedButtonAtIndexAction = {(alertView:UIAlertView, buttonIndex:Int) in
-                    
-                    if let url = NSURL(string:appUrl) {
-                        UIApplication.sharedApplication().openURL(url);
+        let url = NSURL(string:versionUrl);
+        let request = NSMutableURLRequest();
+        request.URL = url;
+        let data = try? NSURLConnection.sendSynchronousRequest(request, returningResponse:nil);
+        if let data = data {
+            let updateVerString = String(data:data, encoding:NSUTF8StringEncoding);
+            
+            if updateVerString == nil {
+                return;
+            }
+            
+            let versionJson = Json.parse(updateVerString!)
+            let updateVersions = parserVersion(versionJson["version"].asString(""));
+            if updateVersions == nil {
+                return;
+            }
+            
+            let appVersions = parserVersion(NSBundle.mainBundle().objectForInfoDictionaryKey("CFBundleShortVersionString") as? String);
+            if appVersions == nil {
+                return;
+            }
+            
+            var flag = 0;//0不需要更新，1、必须更新，2、非必须更新
+            
+            
+            if updateVersions[0] > appVersions[0] {
+                flag = 1;
+            }else if updateVersions[0] == appVersions[0] {
+                if updateVersions[1] > appVersions[1] {
+                    flag = 1;
+                }else if updateVersions[1] == appVersions[1] {
+                    if updateVersions[2] > appVersions[2] {
+                        flag = 2;
                     }
-                    
-                };
+                }
+            }
+            if (flag == 0) {
+                return;
+            }
+            
+            var updateString:String! = nil;
+            if (flag == 1) {
+                updateString = "应用有新版本，为了正常使用，请更新！";
             }else{
-                alert = UIAlertView(title:"", message:updateString, delegate:nil, cancelButtonTitle:"取消", otherButtonTitles:"确定");
-                alert.clickedButtonAtIndexAction = {(alertView:UIAlertView, buttonIndex:Int) in
-                    if buttonIndex == 1 {
+                updateString = "应用有新版本，是否需要更新！";
+            }
+            updateString = versionJson["message"].asString(updateString);
+            Queue.mainQueue(){
+                var alert:UIAlertView! = nil;
+                if flag == 1 {
+                    
+                    alert = UIAlertView(title:"", message:updateString, delegate:nil, cancelButtonTitle:"确定");
+                    alert.clickedButtonAtIndexAction = {(alertView:UIAlertView, buttonIndex:Int) in
                         
                         if let url = NSURL(string:appUrl) {
                             UIApplication.sharedApplication().openURL(url);
                         }
                         
-                    }
-                };
-            }
-            alert.show();
+                    };
+                }else{
+                    alert = UIAlertView(title:"", message:updateString, delegate:nil, cancelButtonTitle:"取消", otherButtonTitles:"确定");
+                    alert.clickedButtonAtIndexAction = {(alertView:UIAlertView, buttonIndex:Int) in
+                        if buttonIndex == 1 {
+                            
+                            if let url = NSURL(string:appUrl) {
+                                UIApplication.sharedApplication().openURL(url);
+                            }
+                            
+                        }
+                    };
+                }
+                alert.show();
+                
+            };
             
-        };
-        
+        }
     }
-}
     private class func parserVersion(version:String!)->[Int]!{
         if version == nil || version == "" {
             return nil;
         }
         let vs = version.componentsSeparatedByString(".");
-        //    if (vs == nil) {
-        //        return nil;
-        //    }
         
         var arr = [Int](count:3,repeatedValue:0);
         for n in 0 ..< 3 {
@@ -112,20 +133,3 @@ public class EnterpriseUpdate{
     
 }
 
-//+(NSArray*)parserVersion:(NSString*)version{
-//    if (version == nil || [version isEqualToString:@""]) {
-//        return nil;
-//    }
-//    NSMutableArray * arr = [[NSMutableArray alloc] init];
-//    NSArray * vs = [version componentsSeparatedByString:@"."];
-//    if (vs == nil || vs.count < 3) {
-//        return nil;
-//    }
-//    
-//    for (int n=0; n<3; n++) {
-//        [arr addObject:[[NSNumber alloc] initWithInt:[vs[n] intValue]]];
-//    }
-//    return arr;
-//}
-//
-//@end
