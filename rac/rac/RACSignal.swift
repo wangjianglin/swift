17f069d57a0f24e00e44473f38adb43fd6ffc70f
wangjianglin <jianglin1>
2016-07-04 17:27:19 +0800

    add RACSignal.swift file

---
+++ rac/rac/RACSignal.swift
@@ -0,0 +1,16 @@
+//
+//  RACSignal.swift
+//  LinRac
+//
+//  Created by lin on 7/4/16.
+//  Copyright © 2016 lin. All rights reserved.
+//
+
+import ReactiveCocoa
+
+public extension RACSignal{
+    
+    public func sendError(error:String){
+        
+    }
+}
