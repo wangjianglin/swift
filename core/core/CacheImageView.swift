8a6ef4422c34651a1071fba5ad1075655c127a31
wangjianglin <jianglin1>
2017-04-17 15:19:55 +0800

    fix bug

--- core/core/CacheImageView.swift
+++ core/core/CacheImageView.swift
@@ -221,7 +221,7 @@ open class CacheImageView : UIImageView{
     
     
     required public init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: aDecoder);
     }
     
     open override func removeFromSuperview() {
