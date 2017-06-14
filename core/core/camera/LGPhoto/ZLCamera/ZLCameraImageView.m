c01cb6e3bc2d59c1b757013b4c38966713c7e07e
vectorse <vector.s@icloud.com>
2017-06-14 10:15:38 +0800

    //

---
+++ core/core/camera/LGPhoto/ZLCamera/ZLCameraImageView.m
@@ -0,0 +1,57 @@
+//
+//  BQImageView.m
+//  BQCommunity
+//
+//  Created by TJQ on 14-8-5.
+//  Copyright (c) 2014年 beiqing. All rights reserved.
+//
+
+#import "ZLCameraImageView.h"
+#import "UIView+Layout.h"
+//#import "UIImage+ZLPhotoLib.h"
+
+@interface ZLCameraImageView ()
+
+@end
+
+@implementation ZLCameraImageView
+
+
+- (UIImageView *)deleBjView{
+    if (!_deleBjView) {
+        _deleBjView = [[UIImageView alloc] init];
+       
+        _deleBjView.hidden = YES;
+        _deleBjView.userInteractionEnabled = YES;
+          UISwipeGestureRecognizer *_swipe;
+         _swipe = [[UISwipeGestureRecognizer alloc]initWithTarget:self action:@selector(deleImage:)];
+         _swipe.direction = UISwipeGestureRecognizerDirectionUp;
+        [_deleBjView addGestureRecognizer:_swipe];
+       // _pan = [[UIPanGestureRecognizer alloc]initWithTarget:self action:@selector(deleImage:)];
+       
+      //  [_deleBjView addGestureRecognizer:_pan];
+       // [_deleBjView addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(deleImage:)]];
+        [self addSubview:_deleBjView];
+    }
+    return _deleBjView;
+}
+
+- (void)setEdit:(BOOL)edit{
+    self.deleBjView.hidden = NO;
+}
+
+- (id)initWithFrame:(CGRect)frame{
+    if (self = [super initWithFrame:frame]) {
+        self.userInteractionEnabled = YES;
+    }
+    return self;
+}
+
+#pragma mark 删除图片
+- (void) deleImage : ( UISwipeGestureRecognizer *) tap{
+    if ([self.delegatge respondsToSelector:@selector(deleteImageView:)]) {
+        [self.delegatge deleteImageView:self];
+    }
+}
+
+@end
